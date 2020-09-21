081ef360872efab8a8bede54b5c52bee6160d9e0
sai-myne <ahlusir@gmail.com>
2020-09-21 14:49:47 +0800

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000/'
+    uri: 'https://tranquil-tor-78146.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
