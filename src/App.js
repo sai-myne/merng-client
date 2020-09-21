b9c8a8ccda6706b563e1dadffb766fa9105b17f7
sai-myne <ahlusir@gmail.com>
2020-09-21 13:26:28 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
+import './App.css';
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import MenuBar from './components/MenuBar'
+import SinglePost from './pages/SinglePost'
+
+import { AuthProvider } from './context/auth'
+import AuthRoute from './util/AuthRoute'
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </Container>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
